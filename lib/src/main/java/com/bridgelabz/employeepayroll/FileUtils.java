01e0d33c7ce5a3194e76d9ca488d3b36b9f0a3c2
srinadh1304 <srinadh1304@gmail.com>
2021-09-19 20:05:09 +0530

    ADDED: Ability to demonstrate file operations

---
+++ lib/src/main/java/com/bridgelabz/employeepayroll/FileUtils.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.employeepayroll;
+import java.io.File;
+
+public class FileUtils {
+	public static boolean deleteFolder(File file) {
+		for (File subFile : file.listFiles()) {
+			if (subFile.isDirectory()) {
+				deleteFolder(subFile);
+			} 
+			else {
+				subFile.delete();
+			}
+		}
+		return file.delete();
+	}
+}
