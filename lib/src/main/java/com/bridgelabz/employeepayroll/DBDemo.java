7193fab2358b92013c962bd1823d7bc77ebbeae3
srinadh1304 <srinadh1304@gmail.com>
2021-09-26 20:46:34 +0530

    REFACTPORED: given correct password

--- lib/src/main/java/com/bridgelabz/employeepayroll/DBDemo.java
+++ lib/src/main/java/com/bridgelabz/employeepayroll/DBDemo.java
@@ -9,18 +9,9 @@ public class DBDemo {
 	public static void main(String [] args) {
 		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
 		String userName = "root";
-		String password = "Root$241";
+		String password = "Perfios@2021";
 		Connection connection;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("Driver loaded!");
-		}
-		catch(ClassNotFoundException e) {
-			throw new IllegalStateException("Cannot find the driver in the classpath",e);
-		}
-		
 		listDrivers();
-	
 		try {
 			System.out.println("Connecting to database:"+jdbcURL);
 			connection =  DriverManager.getConnection(jdbcURL,userName,password);
